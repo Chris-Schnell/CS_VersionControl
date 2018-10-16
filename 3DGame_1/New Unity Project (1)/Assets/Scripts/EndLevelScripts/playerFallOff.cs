1b95f2361d46d7673fa8a534b28946f24f929be8
Christopher Schnell <tylerschnell123@gmail.com>
2018-10-16 15:45:02 -0700

    Level Ends if players falls off

---
+++ 3DGame_1/New Unity Project (1)/Assets/Scripts/EndLevelScripts/playerFallOff.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement;
+
+public class playerFallOff : MonoBehaviour {
+
+    public Transform player;
+
+	// Use this for initialization
+	
+	
+	// Update is called once per frame
+	void Update () {
+		if (player.position.y <= -25)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+	}
+}
