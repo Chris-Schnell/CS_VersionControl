8299a628e89ad6389b92e22e66ecca93c959c3b2
Christopher Schnell <tylerschnell123@gmail.com>
2018-10-22 14:32:06 -0700

    Physics Update, ChangeColorScript, Level3 Begun

---
+++ 3DGame_1/New Unity Project (1)/Assets/Scripts/Collisions/changeMat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeMat : MonoBehaviour {
+
+    public Material toMat;
+    public SphereCollider ballCol;
+    public Renderer theRen;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider == ballCol)
+        {
+            theRen.material = toMat;
+        }
+    }
+}
