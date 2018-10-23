a18fac980ead3dfdb9f47797da99a8e159ec87f4
Christopher Schnell <tylerschnell123@gmail.com>
2018-10-23 16:22:01 -0700

    Added scripts for powerups

---
+++ 3DGame_1/New Unity Project (1)/Assets/Scripts/Collisions/addPoints.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class addPoints : MonoBehaviour
+{
+
+
+    public GameObject theBall;
+
+    void Update()
+    {
+        if (Mathf.Abs(transform.position.x - theBall.transform.position.x ) < 1)
+        {
+            if ( Mathf.Abs (transform.position.y - theBall.transform.position.y ) < 1)
+            {
+                if (Mathf.Abs(transform.position.z - theBall.transform.position.z) < 1){
+                    Debug.Log("points added");
+                    Destroy(this.gameObject);
+                }
+            }
+        }
+
+    }
+
+
+}
