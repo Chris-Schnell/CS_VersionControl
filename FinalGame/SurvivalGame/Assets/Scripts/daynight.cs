51165f10bac45deaa3465cd336e7e7f2419e7a2a
Christopher Schnell <tylerschnell123@gmail.com>
2018-11-25 11:42:31 -0700

    Added stamina functionality, and daynight script

---
+++ FinalGame/SurvivalGame/Assets/Scripts/daynight.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class daynight : MonoBehaviour {
+
+    [SerializeField] private Light sun;
+    [SerializeField] private float secondsinday = 120f;
+
+    [Range(0, 1)] [SerializeField] private float currentTimeOfDay = 0;
+    private float timeMultiplier = 1f;
+    private float sunInitialIntensity;
+
+    private void Start()
+    {
+        sunInitialIntensity = sun.intensity;
+    }
+
+    private void Update()
+    {
+        UpdateSun();
+        
+        
+        currentTimeOfDay += (Time.deltaTime / secondsinday) / 4;
+
+        if ( currentTimeOfDay > .8 || currentTimeOfDay < .2)
+        {
+            currentTimeOfDay += (Time.deltaTime / secondsinday) * 2;
+        }
+
+
+        if(currentTimeOfDay >= 1)
+        {
+            currentTimeOfDay = 0;
+        }
+    }
+
+    void UpdateSun()
+    {
+        sun.transform.localRotation = Quaternion.Euler((currentTimeOfDay * 360f) - 90, 170, 0);
+    }
+}
---
+++ FinalGame/SurvivalGame/Assets/Scripts/vital.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+
+using UnityEngine.UI;
+
+public class vital : MonoBehaviour {
+
+    public int max = 100;
+    public Slider Health;
+    public Slider Hunger;
+    public Slider Thirst;
+
+
+    public Slider stamina;
+
+    private CharacterController charConroller;
+    private FirstPersonController playerController;
+
+    public void Start()
+    {
+        fillVital(Health);
+        fillVital(Hunger);
+        fillVital(Thirst);
+
+        charConroller = GetComponent<CharacterController>();
+        playerController = GetComponent<FirstPersonController>();
+
+    }
+
+    public void Update()
+    {
+        if (Thirst.value > 0)
+        {
+            lowerVital(Thirst, 5000);
+        }
+        if (Hunger.value > 0)
+        {
+            lowerVital(Hunger, 10000);
+        }
+
+
+        if ( Thirst.value*100 < 25 && Thirst.value * 100 > 0)
+        {
+            lowerVital(Health, 4000);
+        }
+        if (Thirst.value * 100 <= 0)
+        {
+            lowerVital(Health, 2000);
+        }
+
+
+        if (Hunger.value * 100 < 25 && Hunger.value * 100 > 0)
+        {
+            lowerVital(Health, 5000);
+        }
+        if (Hunger.value * 100 <= 0)
+        {
+            lowerVital(Health, 3000);
+        }
+
+        //Stamina
+
+        if (charConroller.velocity.magnitude > 0 && Input.GetKey(KeyCode.LeftShift))
+        {
+            lowerVital(stamina, 20);
+        }
+        else
+        {
+            incVital(stamina, 40);
+        }
+
+        if (stamina.value > max)
+        {
+            stamina.value = max;
+        }
+        else if (stamina.value <= 0)
+        {
+            stamina.value = 0;
+            playerController.m_RunSpeed = playerController.m_WalkSpeed;
+        }
+        else if (stamina.value >= 0)
+        {
+            playerController.m_RunSpeed = playerController.m_RunSpeedNorm;
+        }
+
+    }
+
+    public void lowerVital( Slider vitalToLower, float vitalLowerRate)
+    {
+        vitalToLower.value -= Time.deltaTime / vitalLowerRate * 2;
+    }
+
+    public void fillVital(Slider vitalToFill)
+    {
+        vitalToFill.value = 100;
+    }
+
+    public void incVital(Slider vitalToInc , int amountInc)
+    {
+        vitalToInc.value += Time.deltaTime / amountInc * 2;
+    }
+	
+	
+}
