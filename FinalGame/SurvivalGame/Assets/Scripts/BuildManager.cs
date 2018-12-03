b7b8b3ca85dfcdac58dde477533f13f31923f170
Christopher Schnell <tylerschnell123@gmail.com>
2018-12-02 20:33:03 -0700

    Base of a building script set up

---
+++ FinalGame/SurvivalGame/Assets/Scripts/BuildManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour {
+
+    public GameObject wall;
+    public Transform fpscon;
+    public int iswall = 0;
+
+    public GameObject thisWall;
+
+    // Use this for initialization
+    void Start () {
+        
+	}
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown("b") && iswall == 0)
+        {
+            Vector3 spawnpos = fpscon.transform.position + fpscon.forward * 10;
+            thisWall = GameObject.Instantiate(wall, spawnpos, new Quaternion(0f, 0f, 0f, 0f), fpscon);
+            iswall = 1;
+        }
+        if (Input.GetKeyDown("h") && iswall == 1)
+        {
+            thisWall.transform.parent = null;
+            Debug.Log("hereeeeeee");
+        }
+        }
+	}
+
