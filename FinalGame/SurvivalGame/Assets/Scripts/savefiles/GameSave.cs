e94ecb4f266b2dba389483b92aa175aa80de19da
Christopher Schnell <tylerschnell123@gmail.com>
2018-12-02 23:11:49 -0700

    Added serialization for wood amount

---
+++ FinalGame/SurvivalGame/Assets/Scripts/savefiles/GameSave.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSave : MonoBehaviour {
+
+    public GameObject playerobj;
+
+	// Use this for initialization
+	void Start () {
+        RestoreGame();
+
+	}
+    void RestoreGame()
+    {
+        string p = PlayerPrefs.GetString("PlayerRecources");
+        if (p != null && p.Length > 0)
+        {
+            SaveRecources s = JsonUtility.FromJson<SaveRecources>(p);
+
+            if (s != null)
+            {
+                playerobj.GetComponent<Player>().woodAmount = s.woodSto;
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown("p"))
+        {
+            SaveGame();
+        }
+	}
+
+    void SaveGame()
+    {
+        Player pnow = playerobj.GetComponent<Player>();
+        SaveRecources s = new SaveRecources();
+        s.woodSto = pnow.woodAmount;
+
+        string json = JsonUtility.ToJson(s);
+        PlayerPrefs.SetString("PlayerRecources", json);
+
+
+
+    }
+}
---
+++ FinalGame/SurvivalGame/Assets/Scripts/savefiles/SaveRecources.cs
@@ -0,0 +1,13 @@
+﻿
+
+[System.Serializable]
+public class SaveRecources
+{
+    public int woodSto;
+    public int waterSto;
+    public int foodSto;
+    public int axeSto;
+
+
+}
+
